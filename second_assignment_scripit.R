6ce4cdd2a16a60c72b61579ff7492b0b1276fab4
Nikita Kosholkin <nkosholkin@gmail.com>
2014-08-22 23:34:51 +0300

    final commit with script

---
+++ second_assignment_scripit.R
@@ -0,0 +1,20 @@
+The script for the 2-nd assignment
+
+```{r}
+predictionTesting <- predict(rfFit, workoutTesting)
+workoutTestingN <- workoutTesting
+workoutTestingN$classe <- predictionTesting
+
+pml_write_files = function(x) {
+        n = length(x)
+        for (i in 1:n) {
+                filename = paste0("problem_id_", i, ".txt")
+                write.table(x[i], file = filename, quote = FALSE, row.names = FALSE, 
+                            col.names = FALSE)
+        }
+}
+answers <- workoutTestingN$classe
+
+pml_write_files(answers)
+answers
+```
